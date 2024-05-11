ae8c32e0620fa0fab36751357f40ef0b889029cb
Nonti Mbowane <nonty8@gmail.com>
2024-05-11 22:07:15 +0200

    modified app.js and index.html, styles.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,95 +1,126 @@
 
 class Note {
-    constructor(id, title, text) {
-        this.title = title;
-        this.text = text;
-        this.id = id;
+   constructor(id, title, text) {
+      this.title = title;
+      this.text = text;
+      this.id = id;
     }
 }
-
 class App {
-    constructor() {
-        this.notes = [];
+   constructor() {
+      this.notes = [new Note(1, "test title", "test text")];
 
-        this.$activeForm = document.querySelector(".active-form");
-        this.$inactiveForm = document.querySelector(".inactive-form");
-        this.$noteTitle = document.querySelector(".note-title");
-        this.$noteText = document.querySelector(".note-text");
+      this.$activeForm = document.querySelector(".active-form");
+      this.$inactiveForm = document.querySelector(".inactive-form");
+      this.$noteTitle = document.querySelector("#note-title");
+      this.$noteText = document.querySelector("#note-text");
+      this.$notes = document.querySelector(".notes");
+      this.$form = document.querySelector("#form");
 
-        // getting the cursor focused when active form replaces inactive form
-        this.$noteTitle = document.querySelector("#note-title");
-        this.$noteText = document.querySelector("#note-text");
-        
+      this.addEventListeners();
+      this.displayNotes();
+   }
 
-        this.addEventListeners();
-    }
+   addEventListeners(){
+      document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+      });
 
-    addEventListeners(){
-        document.body.addEventListener("click", (event) => {
-        this.handleFormClick(event);
-        });
-    }
+      this.$form.addEventListener("submit", (event) => {
+         event.preventDefault();
+         const title = this.$noteTitle.value;
+         const text = this.$noteText.value;
+         this.addNote({ title, text });
+         this.closeActiveForm();
+      })
+   }
 
-    handleFormClick(event) {
-        const isActiveFormClickedOn = this.$activeForm.contains(event.target);
-        const isInActiveFormClickedOn = this.$inactiveForm.contains(event.target);
-        const title = this.$noteTitle.value;
-        const text =  this.$noteText.value;
+   handleFormClick(event) {
+      const isActiveFormClickedOn = this.$activeForm.contains(event.target);
+      const isInActiveFormClickedOn = this.$inactiveForm.contains(event.target);
+      const title = this.$noteTitle.value;
+      const text =  this.$noteText.value;
+			
+      if (isInActiveFormClickedOn) {
+         this.openActiveForm();
+      } else if (!isInActiveFormClickedOn && !isActiveFormClickedOn) {
+			this.addNote({ title, text });
+         this.closeActiveForm();
+      }
+   }
 
-        console.log((cuid()));
+   openActiveForm() {
+      this.$inactiveForm.style.display = "none";
+      this.$activeForm.style.display = "block";
+      this.$noteText.focus();
+   }
+   
+   closeActiveForm() {
+      this.$inactiveForm.style.display = "block";
+      this.$activeForm.style.display = "none";
+      this.$noteText.value = "";
+      this.$noteTitle.value = "";
+   }
 
-        if (isInActiveFormClickedOn) {
-            this.openActiveForm();
-        } else if (!isInActiveFormClickedOn && !isActiveFormClickedOn) {
-            this.addNote(title, text);
-            this.closeActiveForm();
-        }
-    }
+   addNote({ title, text }) {
+      if (text != "") {
+         const newNote = new Note(cuid(), title, text);
+         this.notes = [...this.notes, newNote];
+         this.displayNotes();
+      }
+   }
 
-    closeActiveForm() {
-        this.$inactiveForm.style.display = "block";
-        this.$activeForm.style.display = "none";
-        this.$noteText.value = "";
-        this.$noteTitle.value = "";
-    }
-    
+   editNote(id, { title, text }) {
+      this.notes = this.notes.map((note) => {
+         if (note.id == id) {
+            note.title = title;
+            note.text = text;
+         }
+         return note;
+      });
+   }
 
-    openActiveForm() {
-        this.$inactiveForm.style.display = "none";
-        this.$activeForm.style.display = "block";
-        this.$noteText.focus();
-    }
-    
-    addNote({ title, text }) {
-        if (!text == "") {
-            const newNote = new Note(cuid(), title, text);
-            this.notes = [...this.notes, newNote];
-            this.displayNotes();
-        }
+   displayNotes() {
+      this.$notes.innerHTML = this.notes.map((note) =>
+         `
+         <div class="note" id="${note.id}">
+         	<span class="material-symbols-outlined check-circle">check_circle</span>
+          		<div class="title">${note.title}</div>
+          		<div class="text">${note.text}</div>
+          	<div class="note-footer">
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon"0>add_alert</span>
+              		<span class="tooltip-text">Remind me</span>
+            	</div>
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon">person_add</span>
+              		<span class="tooltip-text">Collaborator</span>
+            	</div>
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon">palette</span>
+              		<span class="tooltip-text">Change Color</span>
+            	</div>
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon">image</span>
+              		<span class="tooltip-text">Add Image</span>
+            	</div>
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon">archive</span>
+              		<span class="tooltip-text">Archive</span>
+            	</div>
+            	<div class="tooltip">
+              		<span class="material-symbols-outlined hover small-icon">more_vert</span>
+              		<span class="tooltip-text">More</span>
+            	</div>
+          	</div>
+			</div>
+         `
+      ).join("");
+   }
 
-    }
-
-    editNote(id, { title, text }) {
-        this.notes = this.notes.map((note) => {
-            if (note.id == id) {
-                note.title = title;
-                note.text = text;
-            }
-            return note;
-        });
-    }
-
-    displayNotes() {
-        this.notes.map(note => console.log(`
-        ID: ${note.id}
-        Title: ${note.title}
-        Text: ${note.text}
-        `));
-    }
-
-    deleteNote(id) {
-        this.notes = this.notes.filter(note => note.id != id);
-    }
+   deleteNote(id) {
+      this.notes = this.notes.filter(note => note.id != id);
+   }
 
 }
 
